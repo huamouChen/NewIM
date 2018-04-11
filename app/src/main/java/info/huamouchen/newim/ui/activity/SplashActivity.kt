8f01c0b51212f1bb09c2fd94b320629c57d23f50
Evan <hanwu5728@126.com>
2018-04-11 18:00:24 +0800

    Initial commit

---
+++ app/src/main/java/info/huamouchen/newim/ui/activity/SplashActivity.kt
@@ -0,0 +1,17 @@
+package info.huamouchen.newim.ui.activity
+
+import android.app.Activity
+import android.os.Bundle
+import info.huamouchen.newim.R
+
+/**
+ * Created by Rex on 2018/4/11.
+ * Email chenhm4444@gmail.com
+ */
+class SplashActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
---
+++ app/src/main/java/info/huamouchen/newim/ui/fragment/MineFragment.kt
@@ -0,0 +1,11 @@
+package info.huamouchen.newim.ui.fragment
+
+import android.support.v4.app.Fragment
+
+/**
+ * Created by Rex on 2018/4/11.
+ * Email chenhm4444@gmail.com
+ */
+class MineFragment : Fragment() {
+
+}
